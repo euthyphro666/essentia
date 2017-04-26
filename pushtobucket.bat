6d02cc7ec1fb561e2dccbdc3e6378b5e967f3f00
elitej13 <joshhess13@gmail.com>
2017-04-26 02:01:40 -0600

    Fixed social banner formatting issues, also officially swapped out test home page with homepage. Onto about page and more content!

--- pushtobucket.bat
+++ pushtobucket.bat
@@ -1,6 +1,1 @@
-@Echo off
-
-:: Verify this address with local google app engine sdk path
-C:\WINDOWS\system32\cmd.exe /k ""C:\Users\Josh\AppData\Local\Google\Cloud SDK\cloud_env.bat""
-
-gsutil rsync -rd -x \.git . gs://joshhess.info
+gsutil rsync -r -x \.git \.idea . gs://joshhess.info/
